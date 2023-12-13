b1b44cec10989bbe57bf785f9e3c5e38c40ebea6
lmsand <lmsand99@icloud.com>
2023-12-12 23:16:07 -0600

    Detail View

---
+++ AppleFrameworks/FrameworkDetailView.swift
@@ -0,0 +1,53 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameworks
+//
+//  Created by lexi sanders on 12/12/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            
+            HStack {
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundStyle(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+
+            }
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
+    }
+}
+
+#Preview {
+    FrameworkDetailView(framework: MockData.sampleFramework)
+}
